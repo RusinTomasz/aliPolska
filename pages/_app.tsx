d903e6213812b09a15597c45cf981a6875ed3f2c
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-24 18:12:45 +0100

    boilerplate folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,12 @@
 import "../styles/globals.css";
 import { FC } from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import { createGlobalStyle } from "styled-components";
+import GlobalStyle from "./app/styles/GlobalStyle";
 
 /* Redux */
 import { wrapper } from "../store/store";
 import { Provider } from "react-redux";
 import store from "../store/store";
-
-const GlobalStyle = createGlobalStyle`
-  h1,h2,h3,h4,h5,h6 {
-    font-family: 'Montserrat', sans-serif;
-  }
-  h1,h2 {
-    font-weight: 800;
-  }
-  h3,h4,h5,h6 {
-    font-weight: bold;
-  }
-  h1 {
-    font-size: 48px;
-    line-height: 50px;
-  }
-  h3 {
-    font-size: 18px;
-    line-height: 25px;
-  }
-  body {
-    font-family: 'Open Sans', sans-serif;
-    font-weight: 300;
-    font-size: 20px;
-    line-height: 30px;
-  }
-  a {
-    font-family: 'Open Sans', sans-serif;
-    font-weight: bold;
-  }
-`;
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
