0f51b47d1949215425d68db87c0d735de2448f21
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-24 18:09:42 +0100

    add basic global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,32 @@ import { Provider } from "react-redux";
 import store from "../store/store";
 
 const GlobalStyle = createGlobalStyle`
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Montserrat', sans-serif;
+  }
+  h1,h2 {
+    font-weight: 800;
+  }
+  h3,h4,h5,h6 {
+    font-weight: bold;
+  }
+  h1 {
+    font-size: 48px;
+    line-height: 50px;
+  }
+  h3 {
+    font-size: 18px;
+    line-height: 25px;
+  }
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-color: purple;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 30px;
+  }
+  a {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: bold;
   }
 `;
 
