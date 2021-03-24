63f0b7ec5c58fd904c0914cca4b4d109681e5a14
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-24 20:20:52 +0100

    add gradients to styles variables

--- app/styles/variables.tsx
+++ app/styles/variables.tsx
@@ -1,7 +1,23 @@
 interface Colors {
-  violette: string;
+  purple: string;
+  red: string;
+  orange: string;
+}
+interface Gradients {
+  redOrange: string;
 }
 
 export const colors: Colors = {
-  violette: "#312d3b",
+  purple: "#C875FF",
+  red: "#ff4747",
+  orange: "#ff9a00",
 };
+
+export const gradients: Gradients = {
+  redOrange:
+    "linear-gradient(94deg, rgb(255, 10, 10) 0%, rgb(255, 117, 57) 100%)",
+};
+
+
+
+
