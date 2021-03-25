81cfedb917dfa5eefac6c437ac049291e9bb763e
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-25 19:31:47 +0100

    PageTitle styled component

--- pages/aktualnosci/index.tsx
+++ pages/aktualnosci/index.tsx
@@ -1,10 +1,9 @@
 import Head from "next/head";
-import { useSelector } from "react-redux";
-import { postsSelectors } from "../../store/selectors";
 import { wrapper } from "../../store/store";
 import * as postActions from "../../store/actions/postActions";
 import { fetchPosts } from "../../store/services/post.service";
 import Link from "next/link";
+import PageTitle from "../../app/styles/shared/pageTitle/PageTitle";
 
 const Blog = (props): JSX.Element => {
   return (
@@ -12,16 +11,7 @@ const Blog = (props): JSX.Element => {
       <Head>
         <title>Blog</title>
       </Head>
-      <h1>Blog</h1>
-      <Link href="/">FrontPage</Link>
-      <div>
-        {props.posts &&
-          props.posts.map((post) => (
-            <div key={post.id}>
-              <h2>{post.Title}</h2>
-            </div>
-          ))}
-      </div>
+      <PageTitle>Najnowsze aktualności</PageTitle>
     </>
   );
 };
