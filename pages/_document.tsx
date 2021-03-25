d209be54a1c0ae7d95f51d1e41e5dcbcdcf8342c
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-25 06:08:43 +0100

    favicons and homepage title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,7 +54,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;800&family=Open+Sans:wght@300;400;600;700&display=swap"
             rel="stylesheet"
             media="print"
-            onLoad="this.media='all'"
+            // onLoad="this.media='all'"
           />
           <noscript>
             <link
